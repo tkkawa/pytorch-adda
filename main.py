4c84ba19d45a49c3d497a2d130e96b727720d953
tkkawa <tkm.kawa@gmail.com>
2021-08-30 11:18:02 +0900

    modify main and test

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
-"""Main script for ADDA."""
-
 import params
 from core import eval_src, eval_tgt, train_src, train_tgt
 from models import Discriminator, LeNetClassifier, LeNetEncoder
 from utils import get_data_loader, init_model, init_random_seed
+from torch.utils.data import DataLoader, Dataset, random_split
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
 
 if __name__ == '__main__':
     # init random seed
     init_random_seed(params.manual_seed)
 
     # load dataset
-    src_data_loader = get_data_loader(params.src_dataset)
-    src_data_loader_eval = get_data_loader(params.src_dataset, train=False)
-    tgt_data_loader = get_data_loader(params.tgt_dataset)
-    tgt_data_loader_eval = get_data_loader(params.tgt_dataset, train=False)
+    #src_data_loader = get_data_loader(params.src_dataset)
+    #src_data_loader_eval = get_data_loader(params.src_dataset, train=False)
+    #tgt_data_loader = get_data_loader(params.tgt_dataset)
+    #tgt_data_loader_eval = get_data_loader(params.tgt_dataset, train=False)
 
+    # Dataset を作成する。
+    transform = transforms.Compose([transforms.Grayscale(num_output_channels=1), transforms.Resize((28, 28)), transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))])
+    src_dataset = ImageFolder("datasets2/animal_face_dataset/resized", transform)
+    tgt_dataset = ImageFolder("datasets2/human_dataset/resized", transform)
+    train_ratio = 0.8
+    src_train_size = int(train_ratio * len(src_dataset))
+    src_val_size  = len(src_dataset) - src_train_size   
+    src_data_size  = {"train":src_train_size, "val":src_val_size}
+    print(src_data_size)
+    tgt_train_size = int(train_ratio * len(tgt_dataset))
+    tgt_val_size  = len(tgt_dataset) - tgt_train_size   
+    tgt_data_size  = {"train":tgt_train_size, "val":tgt_val_size}
+    print(tgt_data_size)
+    
+    src_data_train, src_data_val = random_split(src_dataset, [src_train_size, src_val_size])
+    src_data_loader = DataLoader(src_data_train, batch_size=3, shuffle=True)
+    src_data_loader_eval = DataLoader(src_data_val, batch_size=2, shuffle=True)
+
+    tgt_data_train, tgt_data_val = random_split(tgt_dataset, [tgt_train_size, tgt_val_size])
+    tgt_data_loader = DataLoader(tgt_data_train, batch_size=2, shuffle=True)
+    tgt_data_loader_eval = DataLoader(tgt_data_val, batch_size=, shuffle=True)
+    #print(type(dataloader))
+    print(type(src_data_loader))
+    print(type(tgt_data_loader))
     # load models
     src_encoder = init_model(net=LeNetEncoder(),
                              restore=params.src_encoder_restore)
@@ -62,6 +87,6 @@ if __name__ == '__main__':
     # eval target encoder on test set of target dataset
     print("=== Evaluating classifier for encoded target domain ===")
     print(">>> source only <<<")
-    eval_tgt(src_encoder, src_classifier, tgt_data_loader_eval)
+    eval_tgt(src_encoder, src_classifier, src_data_loader_eval)
     print(">>> domain adaption <<<")
     eval_tgt(tgt_encoder, src_classifier, tgt_data_loader_eval)
---
+++ test.py
@@ -0,0 +1,47 @@
+import torch
+import torchvision.utils
+from torch.utils.data import DataLoader, Dataset, random_split
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from utils import make_variable
+from utils import get_data_loader, init_model, init_random_seed
+from models import Discriminator, LeNetClassifier, LeNetEncoder
+import params
+
+def eval_target(encoder, classifier, data_loader):
+    #encoder.eval()
+    #classifier.eval()
+    res = 0
+
+    #classes = ['cat', 'fox', 'gorilla', 'raccoon']
+    for (images, labels) in data_loader:
+        #imshow(torchvision.utils.make_grid(images))
+        images = make_variable(images, volatile=True)
+        labels = make_variable(labels).squeeze_()
+        print(labels)
+        preds = classifier(encoder(images))
+        _, predicted = torch.max(preds, 1)
+        print(predicted)
+        if labels == predicted:
+            res += 1
+        print("---------")
+
+    print('acc', res / len(data_loader)
+
+src_encoder = init_model(net=LeNetEncoder(),
+                             restore=params.src_encoder_restore)
+src_classifier = init_model(net=LeNetClassifier(),
+                            restore=params.src_classifier_restore)
+transform = transforms.Compose([transforms.Grayscale(num_output_channels=1), transforms.Resize((28, 28)), transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))])
+tgt_dataset = ImageFolder("datasets2/human_dataset/resized", transform)
+
+train_ratio = 0.8
+tgt_train_size = int(train_ratio * len(tgt_dataset))
+tgt_val_size  = len(tgt_dataset) - tgt_train_size   
+tgt_data_size  = {"train":tgt_train_size, "val":tgt_val_size}
+print(tgt_data_size)
+
+tgt_data_train, tgt_data_val = random_split(tgt_dataset, [tgt_train_size, tgt_val_size])
+tgt_data_loader = DataLoader(tgt_data_train, batch_size=1, shuffle=True)
+tgt_data_loader_eval = DataLoader(tgt_data_val, batch_size=1, shuffle=True)
+eval_target(src_encoder, src_classifier, tgt_data_loader_eval)
