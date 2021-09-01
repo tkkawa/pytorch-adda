65f5798411ee89488d5559acadd288d85b7bd258
tkkawa <tkm.kawa@gmail.com>
2021-09-01 19:43:01 +0900

    Update lenet.py

--- models/lenet.py
+++ models/lenet.py
@@ -15,25 +15,25 @@ class LeNetEncoder(nn.Module):
 
         self.encoder = nn.Sequential(
             # 1st conv layer
-            # input [1 x 28 x 28]
-            # output [20 x 12 x 12]
-            nn.Conv2d(1, 20, kernel_size=5),
+            # input [1 x 100 x 100]
+            # output [20 x 24 x 24]
+            nn.Conv2d(1, 20, kernel_size=4, stride=2),
             nn.MaxPool2d(kernel_size=2),
             nn.ReLU(),
             # 2nd conv layer
-            # input [20 x 12 x 12]
-            # output [50 x 4 x 4]
-            nn.Conv2d(20, 50, kernel_size=5),
+            # input [20 x 24 x 24]
+            # output [50 x 5 x 5]
+            nn.Conv2d(20, 50, kernel_size=4, stride=2),
             nn.Dropout2d(),
             nn.MaxPool2d(kernel_size=2),
             nn.ReLU()
         )
-        self.fc1 = nn.Linear(50 * 4 * 4, 500)
+        self.fc1 = nn.Linear(50 * 5 * 5, 500)
 
     def forward(self, input):
         """Forward the LeNet."""
         conv_out = self.encoder(input)
-        feat = self.fc1(conv_out.view(-1, 50 * 4 * 4))
+        feat = self.fc1(conv_out.view(-1, 50 * 5 * 5))
         return feat
 
 
@@ -43,7 +43,7 @@ class LeNetClassifier(nn.Module):
     def __init__(self):
         """Init LeNet encoder."""
         super(LeNetClassifier, self).__init__()
-        self.fc2 = nn.Linear(500, 10)
+        self.fc2 = nn.Linear(500, 4)
 
     def forward(self, feat):
         """Forward the LeNet classifier."""
